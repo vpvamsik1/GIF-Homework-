0acd837ab29dd2dd1c4f7621d8adba0185c32950
Vamsi Krishna <vpvamsik1@gmail.com>
2018-07-12 18:14:49 -0700

    This is my HW

---
+++ logic.js
@@ -0,0 +1,87 @@
+        var heroes = ["Superman", "Batman", "Spiderman"];
+        
+        function makeButtons() {
+            $("#heroButtons").empty();
+            for(var i=0; i<heroes.length; i++){
+            var b = $("<button>");
+            b.addClass("hero");
+            b.addClass("btn btn-success");
+            b.attr("data-animal", heroes[i]);
+            b.text(heroes[i]);
+            $("#heroButtons").append(b);
+            }
+        }
+        makeButtons();
+        
+        $("#B1").on("click", function(event){
+            
+            event.preventDefault();
+            var hero = $("#text1").val();
+            heroes.push(hero);
+            makeButtons();
+        });
+
+        function showGifs() {
+            var hero = $(this).attr("data-animal");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+            $.ajax({
+                url:queryURL,
+                method: "GET"
+            }).then(function(response){
+                console.log(queryURL);
+                console.log(response);
+
+                var results = response.data;
+
+                for (var i=0; i < results.length; i++){
+
+                    var heroDiv= $("<div>");
+
+                    heroDiv.attr('class', 'gif');
+
+                    var p = $("<p>").html("Rating: " + results[i].rating);
+                        
+                    var heroImg = $("<img>");
+
+                    heroImg.attr("src", results[i].images.fixed_height.url);
+
+                    heroImg.attr("data-animate", results[i].images.fixed_height.url);
+
+                    heroImg.attr("data-still", results[i].images.fixed_height_still.url);
+
+                    heroImg.attr("data-state", "animate");
+
+                    heroImg.attr("class", "imgGif");
+
+                    heroDiv.append(p);
+
+                    heroDiv.append(heroImg);
+
+                    $("#heroes").append(heroDiv);
+                    
+
+                }
+            $(".gif").on("click", '.imgGif', function() {
+            // alert("hello");
+            var state = $(this).attr("data-state");
+            // console.log(state);
+
+            if (state === "animate") {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            } else {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            }
+            console.log('hi');
+        });
+
+
+            })
+
+        }
+
+
+
+        $(document).on("click", ".hero", showGifs);
